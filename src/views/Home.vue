6a1d5efafc75a1bcc38e877027358a5295636233
Rubén Abad <rubenabad89@gmail.com>
2020-11-28 05:39:30 +0100

    Remove unused what to but section from home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -277,91 +277,11 @@
         <h3 class="machine-type-title">Cafeteras Gronenberg</h3>
       </article>
     </div>
-    <h1>¿Qué cafetera comprar?</h1>
-    <p>
-      Hoy en día existe una amplia variedad de cafeteras en el mercado
-      disponibles para nuestro uso y disfrute. Dependiendo del tipo de café, el
-      tipo de elaboración y cuerpo y sabor que desemos obtener nos interesará un
-      tipo de cafetera u otra.
-    </p>
-    <h2>Cafetera Express</h2>
-    <div class="section-body">
-      <a class="section-image" href="https://amzn.to/32onA0z" rel="nofollow">
-        <img
-          src="../assets/home_express.webp"
-          alt="Cafetera express"
-          width="100%"
-        />
-      </a>
-      <p>
-        Si lo que quieres es disfrutar del buen sabor de un
-        <b>café de cafetería o restaurante</b>, la cafetera express es la que
-        estás buscando.
-        <br />
-        <br />
-        Ésta típica <b>cafetera de bar</b> es capaz de prepar un
-        <i>café intenso</i> y una <i>crema deliciosa</i>
-        que nos daran un buen empuje de energia para afrontar el día.
-        <br />
-        <br />
-        Aquí tienes un listado de las mejores ofertas en cafeteras express de
-        2020:
-      </p>
-    </div>
-    <AfiliateRow
-      :links="[
-        '//rcm-eu.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=micafetera-21&language=es_ES&o=30&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B0763N7WMP&linkId=52121ebc26d3c598a3ff2a419e513cea',
-        '//rcm-eu.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=micafetera-21&language=es_ES&o=30&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B06WGTZ874&linkId=917a399b37cc2ec89b0fb4f708d03026',
-        '//rcm-eu.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=micafetera-21&language=es_ES&o=30&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B07YFYS364&linkId=0f7fd2854005680b4a4a8c7992114e6e',
-        '//rcm-eu.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=micafetera-21&language=es_ES&o=30&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B07KJ5K22S&linkId=1b3f0e81c469c776c2990a3b04db0e84',
-        '//rcm-eu.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=micafetera-21&language=es_ES&o=30&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B07XBY42XM&linkId=49d1c0d0a39a3bcdc6dfb5f66c92e070',
-        '//rcm-eu.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=micafetera-21&language=es_ES&o=30&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B01M9BF86K&linkId=8a26e67408d69ef1943e41b372f26851',
-      ]"
-    />
-    <h2>Cafetera automática</h2>
-    <div class="section-body">
-      <a class="section-image" href="https://amzn.to/32onA0z" rel="nofollow">
-        <img
-          src="../assets/home_automatic.webp"
-          alt="Cafetera express"
-          width="100%"
-        />
-      </a>
-      <p>
-        Las <b>cafeteras automáticas</b> o también conocidas como
-        <b>cafeteras superautomáticas</b> son la máxima expresión del sabor del
-        café y la comódidad en su preparación.
-        <br />
-        <br />
-        Este tipo de máquinas de café tienen incorporadas todos los componentes
-        necesarios para hacer
-        <b>el mejor café con solo pulsar un solo botón</b>.
-        <br />
-        <br />
-        Son las cafeteras de más <b>alta gama</b> y por ello también las mas
-        caras del mercado.
-        <br />
-        <br />
-        A continuación dispones de una lista par comprar las mejores cafeteras
-        automáticas de 2020:
-      </p>
-    </div>
-    <AfiliateRow
-      :links="[
-        '//rcm-eu.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=micafetera-21&language=es_ES&o=30&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B00MZ2F3Z8&linkId=82ff3749c0aa4da7e08d9f511d8cdff7',
-        '//rcm-eu.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=micafetera-21&language=es_ES&o=30&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B002OHDBLW&linkId=816058d92d045bfcf37f396a3bba0f4f',
-        '//rcm-eu.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=micafetera-21&language=es_ES&o=30&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B07MMSHC4R&linkId=4871002b0e857b4ee312c57e7226de39',
-        '//rcm-eu.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=micafetera-21&language=es_ES&o=30&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B00OT1CO0U&linkId=c0568362c94e7a2ebcf1d448b00ad048',
-        '//rcm-eu.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=micafetera-21&language=es_ES&o=30&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B00LPG2LLY&linkId=050ac3665a6d99b0d71d9ca3e2bd792c',
-        '//rcm-eu.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=micafetera-21&language=es_ES&o=30&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B00I3YL5T0&linkId=aad0c963ac2e872fa8cb4488bd3d6b50',
-      ]"
-    />
   </Frame>
 </template>
 
 <script>
 import Frame from '@/components/Frame.vue';
-import AfiliateRow from '@/components/AfiliateRow.vue';
 import Cluster from '@/components/Cluster.vue';
 import AffiliateGrid from '@/components/AffiliateGrid.vue';
 
@@ -369,7 +289,6 @@ export default {
   name: 'Home',
   components: {
     Frame,
-    AfiliateRow,
     Cluster,
     AffiliateGrid,
   },
@@ -379,23 +298,6 @@ export default {
 <style scoped>
 p {
   line-height: 150%;
-}
-.section-body {
-  display: flex;
-  flex-direction: column;
-}
-.section-image {
-  width: 100%;
-  margin-right: 16px;
-}
-@media only screen and (min-width: 768px) {
-  .section-body {
-    display: flex;
-    flex-direction: row;
-  }
-  .section-image {
-    width: 50%;
-  }
 }
 .machine-types {
   display: flex;
